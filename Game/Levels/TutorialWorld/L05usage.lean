077a3c4415473b26f192b6cd6b6a270bd7055afe
Marc Masdeu <marc.masdeu@gmail.com>
2024-05-27 16:03:00 +0000

    Small rewrite of Usage level.

--- Game/Levels/TutorialWorld/L05usage.lean
+++ Game/Levels/TutorialWorld/L05usage.lean
@@ -4,30 +4,29 @@ open IncidencePlane --hide
 World "TutorialWorld"
 Level 5
 
-Title "Using theorems"
+Title "Using theorems / axioms"
 TheoremTab "∈"
 Introduction
 "
-You will see that two new axioms have been added to the *Axioms* list:
+You will see that two new axioms have been added to the *Axioms* tab in the *Theorems* list:
 
 * line_through_left (P Q : Ω) : P ∈ (line_through P Q)
 
 * line_through_right (P Q : Ω) : Q ∈ (line_through P Q)
 
-**Note the name of the two statements**. Mathematicians sometimes call them 'Lemma 2.1' or 'Hypothesis P6' or something. But
-computer scientists call them `line_through_left` and `line_through_right` because they are easier to use and remember.
+Given two points P and Q, we can form the line passing through them (which is one of the axioms).
+This line is called `line_through P Q`. To encode that it passes through these points, we have
+the two *axioms* above. Just after the name, you see that two parentheses appear, with the variables
+that these take. For each choice of two points, we get a statement that says that the first of them
+belongs to `line_through P Q` (this is what `line_through_left P Q` says), and another corresponding
+statement to say that the second point belongs to the same line (that is, `line_through_right P Q`).
+
 From now on,
-all the statements that appear on this list will be remembered by the computer.
-In this way, we won't have to provide their proofs again.
-Instead, we will use them straightforwardly in case they are handy for solving the following levels.
+all the statements that appear on this list will be remembered by the computer. They will either
+be axioms (which we will find in the *Axioms* tab) or theorems that we have already proved in
+previous levels (and which will become available as you make progress).
 
-Just after the name of the statements, two parentheses appear. Inside them, there are the exact number of variables that are needed to
-put out that statements. Then, the colon `:` symbol introduces the statement as such. In this case, they are very similar to each other.
-What they come to mean is that we can draw a line that passes through two distinct points (P and Q) that lie in a plane (Ω). They symbol `∈`
-is read as *is an element of*. Then, P and Q are elements of the line that passes through the points P and Q. Seems obvious, right? Now, let's
-try to solve this level!
-
-Looking at the goal, you will see the same structure as
+Back to this level, if you look at the goal you will see the same structure as
 the statement `line_through_right (P Q : Point) : Q ∈ (line_through P Q)`.
 Then, we just have to write that statement in a different way!
 Do you remember that the `apply`
